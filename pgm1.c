e75f125a5310ea16297b7b1b863362e06b6defe7
nandigam2005246 <65825929+nandigam2005246@users.noreply.github.com>
2020-05-23 23:34:15 +0530

    Create pgm1.c

---
+++ pgm1.c
@@ -0,0 +1,45 @@
+/*   
+
+        *
+       * *
+      * * *
+     * * * *
+	* * * * *    */
+#include<stdio.h>
+int main ()
+{
+	int n;
+	printf ("Enter the no of elements\n");
+	scanf ("%d",&n);
+	int row,col,space;
+	
+	for (row=1;row<=n;row++)
+	{	
+		if (row!=n)
+		{
+			for (space =n-row;space>0;space--)
+			{
+				printf (" ");
+				
+			}
+		}
+		
+		
+		
+		for (col=row;col>0;col--)
+		
+		{
+			printf ("1 ");
+		}
+	
+		printf ("\n");
+	
+	
+	}	
+	
+	
+	
+	
+	
+	return 0;
+}
